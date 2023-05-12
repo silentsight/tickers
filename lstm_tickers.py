0af875faa2b5b2d5bf4bd7d916840b5973469401
David Matthew <silentsight@live.com>
2023-05-12 16:36:47 -0700

    Added RSI and RSI to model analysis

--- lstm_tickers.py
+++ lstm_tickers.py
@@ -9,13 +9,16 @@ import yfinance as yf
 from scipy.stats import kurtosis, skew
 import seaborn as sns
 import math
+from ta.momentum import rsi
+from ta.trend import macd
 
 SEQUENCE = 60
-PERIOD= 14
+PERIOD = 7
+
 
 def predict_stock(ticker):
     # Fetch historical stock data
-    df = yf.download(ticker,'2015-01-01','2023-05-11')
+    df = yf.download(ticker, '2015-01-01', '2023-05-12')
 
     # Use only close prices
     df = df[['Close']]
@@ -27,7 +30,11 @@ def predict_stock(ticker):
 
     # Add volatility for volatility analysis
     df['Log_Returns'] = np.log(df['Close'] / df['Close'].shift())
-    df['Volatility'] = df['Log_Returns'].rolling(window=21).std() * np.sqrt(252) # annualized volatility
+    df['Volatility'] = df['Log_Returns'].rolling(window=21).std() * np.sqrt(252)  # annualized volatility
+
+    # Calculate RSI and MACD
+    df['RSI'] = rsi(df['Close'])
+    df['MACD'] = macd(df['Close'])
 
     # Plot the data
     df[['Close', 'MA_10', 'MA_30', 'MA_50']].plot(figsize=(10, 4), grid=True)
@@ -36,6 +43,14 @@ def predict_stock(ticker):
 
     df['Volatility'].plot(figsize=(10, 4), grid=True)
     plt.title(f'{ticker} Stock Volatility')
+    plt.show()
+
+    df['RSI'].plot(figsize=(10, 4), grid=True)
+    plt.title(f'{ticker} RSI')
+    plt.show()
+
+    df['MACD'].plot(figsize=(10, 4), grid=True)
+    plt.title(f'{ticker} MACD')
     plt.show()
 
     # Calculate skewness and kurtosis
@@ -59,12 +74,11 @@ def predict_stock(ticker):
     for t in range(1, t_intervals):
         price_list[t] = price_list[t - 1] * daily_returns[t]
 
-    plt.figure(figsize=(10,6))
+    plt.figure(figsize=(10, 6))
     plt.plot(price_list)
     plt.title(f'{ticker} Monte Carlo Simulation')
-    plt.show()
-
-    # Recommendation score based on volatility, skewness, kurtosis, recent performance, trend, and momentum
+    plt.show
+    # Recommendation score based on volatility, skewness, kurtosis, recent performance, trend, momentum, RSI, and MACD
     volatility_threshold = 0.30  # This is an example, you can adjust this value
     skewness_threshold = 0  # This is an example, you can adjust this value
     kurtosis_threshold = 3  # This is an example, you can adjust this value
@@ -72,6 +86,8 @@ def predict_stock(ticker):
     recent_performance_threshold_6_month = 0.20  # 20% up in the past six months
     trend_threshold = 1.05  # Latest closing price is at least 5% above MA_50
     momentum_threshold = 0.05  # Rate of change is at least 5%
+    rsi_threshold = 50  # RSI threshold value
+    macd_threshold = 0  # MACD threshold value
 
     score = 0
     if df['Volatility'].iloc[-1] < volatility_threshold:
@@ -98,6 +114,14 @@ def predict_stock(ticker):
     if momentum > momentum_threshold:
         score += 1
 
+    # Check RSI
+    if df['RSI'].iloc[-1] > rsi_threshold:
+        score += 1
+
+    # Check MACD
+    if df['MACD'].iloc[-1] > macd_threshold:
+        score += 1
+
     # Store final results
     results = pd.DataFrame({
         'Ticker': [ticker],
@@ -108,6 +132,8 @@ def predict_stock(ticker):
         '6-Month Performance': [recent_performance_6_month],
         'Trend': [df['Close'].iloc[-1] / df['MA_50'].iloc[-1] - 1],
         'Momentum': [momentum],
+        'RSI': [df['RSI'].iloc[-1]],
+        'MACD': [df['MACD'].iloc[-1]],
         'Score': [score]
     })
     print(results)
@@ -122,7 +148,6 @@ def predict_stock(ticker):
     # Add another scaler for 'Close' prices only
     close_scaler = MinMaxScaler()
     df[['Close']] = close_scaler.fit_transform(df[['Close']])
-
     # Function to create sequences
     def create_sequences(data, sequence_length):
         x = []
@@ -133,7 +158,7 @@ def predict_stock(ticker):
         return np.array(x), np.array(y).reshape(-1, 1)
 
     # Create sequences
-    sequence_length = SEQUENCE # Adjust sequence length
+    sequence_length = SEQUENCE  # Adjust sequence length
     x, y = create_sequences(data_scaled, sequence_length)
 
     # Split the data into training and testing data
@@ -143,7 +168,7 @@ def predict_stock(ticker):
 
     # Build the CNN-GRU model
     model = Sequential([
-        Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(sequence_length, 6)),
+        Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(sequence_length, 8)),
         MaxPooling1D(pool_size=2),
         GRU(100, activation='relu', return_sequences=True),
         Dropout(0.2),
@@ -165,7 +190,7 @@ def predict_stock(ticker):
     predictions = close_scaler.inverse_transform(predictions)
 
     # Now let's use the model to predict the next 14 days
-    new_df = data_scaled[-sequence_length:].copy()  # shape: (sequence_length, 6)
+    new_df = data_scaled[-sequence_length:].copy()  # shape: (sequence_length, 8)
     forecast = []
 
     df['Close'] = close_scaler.inverse_transform(df[['Close']])
@@ -175,8 +200,8 @@ def predict_stock(ticker):
         predicted_price = model.predict(new_df_scaled)  # shape: (1, 1)
         # Propagate the last features
         last_features = new_df[-1, 1:]
-        new_prediction = np.concatenate([predicted_price, last_features.reshape(1, -1)], axis=1)  # shape: (1, 6)
-        new_df = np.concatenate([new_df[1:], new_prediction])  # shape: (sequence_length, 6)
+        new_prediction = np.concatenate([predicted_price, last_features.reshape(1, -1)], axis=1)  # shape: (1, 8)
+        new_df = np.concatenate([new_df[1:], new_prediction])  # shape: (sequence_length, 8)
 
         forecast.append(predicted_price[0])
 
@@ -204,11 +229,13 @@ def predict_stock(ticker):
     result['Percentage Change'] = result['Forecast'].pct_change() * 100
     print(result)
 
-    # Plot the actual, training, testing and forecasted prices
+    # Plot the actual, training, testing, and forecasted prices
     plt.figure(figsize=(12, 8))
-    plt.plot(df.index[sequence_length:sequence_length+len(y_train)], close_scaler.inverse_transform(y_train.reshape(-1, 1)), color='blue', label='Training Data')
-    plt.plot(df.index[sequence_length+len(y_train)+1:], close_scaler.inverse_transform(y_test.reshape(-1,1)), color='green', label='Testing Data')
-    plt.plot(df.index[sequence_length+len(y_train)+1:], predictions, color='red', label='Predicted Price')
+    plt.plot(df.index[sequence_length:sequence_length + len(y_train)],
+             close_scaler.inverse_transform(y_train.reshape(-1, 1)), color='blue', label='Training Data')
+    plt.plot(df.index[sequence_length + len(y_train) + 1:],
+             close_scaler.inverse_transform(y_test.reshape(-1, 1)), color='green', label='Testing Data')
+    plt.plot(df.index[sequence_length + len(y_train) + 1:], predictions, color='red', label='Predicted Price')
     plt.plot(forecast_week, color='orange', label='Forecasted Price')
     plt.title(f'{ticker} Stock Price Prediction')
     plt.xlabel('Date')
